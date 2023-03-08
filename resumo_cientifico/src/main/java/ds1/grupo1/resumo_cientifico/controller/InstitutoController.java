628a95be537756ec80d9ee7d8a726be62d3f0149
Arthur Estephano Lemos de Moura <arthurlemos98@hotmail.com>
2023-03-07 21:55:42 -0300

    Updating institute API and backend
    Updating institute API and backend

--- resumo_cientifico/src/main/java/ds1/grupo1/resumo_cientifico/controller/InstitutoController.java
+++ resumo_cientifico/src/main/java/ds1/grupo1/resumo_cientifico/controller/InstitutoController.java
@@ -1,12 +1,15 @@
 package ds1.grupo1.resumo_cientifico.controller;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ds1.grupo1.resumo_cientifico.controller.request.InstitutoRs;
 import ds1.grupo1.resumo_cientifico.model.Instituto;
 import ds1.grupo1.resumo_cientifico.repository.InstitutoRepository;
 
@@ -21,13 +24,55 @@ public class InstitutoController {
     }
 
     @GetMapping("/list")
-    public List<Instituto> getInstitutos() {
-        return institutoRepository.findAll();
+    public List<InstitutoRs> getInstitutos() {
+        List<Instituto> institutos = institutoRepository.findAll();
+
+        List<InstitutoRs> irs = new ArrayList<InstitutoRs>();
+        for (Instituto instituto: institutos){
+            InstitutoRs i = new InstitutoRs();
+            i.setAcronimo(instituto.getAcronimo());
+            i.setNome(instituto.getNome());
+            i.setId(instituto.getId());
+            irs.add(i);
+        }
+
+        return irs;
     }
 
     @PostMapping("/")
-    public void gravar(@RequestBody Instituto instituto){
+    public void gravar(@RequestBody InstitutoRs institutoRs){
+        Instituto instituto = new Instituto ();
+        instituto.setAcronimo(institutoRs.getAcronimo());
+        instituto.setNome(institutoRs.getNome());
+        
         institutoRepository.save(instituto);
     }
-    
+
+    @GetMapping("/{id}")
+    public void deleteInstituto(@PathVariable("id") Long id) throws Exception {
+        var i = institutoRepository.findById(id);
+
+        if (i.isPresent()){
+            Instituto instituto = i.get();
+            institutoRepository.delete(instituto);
+        } else {
+            throw new Exception("Id não encontrado.");
+        }
+        
+    }
+
+    @GetMapping("/name/{nome}")
+    public List<Instituto> getInstitutoByNome(@PathVariable("nome") String nome){
+        return institutoRepository.findByNomeContaining(nome);
+    }
+
+    @GetMapping("/acronym/{acronimo}")
+    public List<Instituto> getInstitutoByAcronimo(@PathVariable("acronimo") String acronimo){
+        return institutoRepository.findByAcronimoContaining(acronimo);
+    }
+
+    @GetMapping("/name-acronym/{busca}")
+    public List<Instituto> getInstitutoByNomeOrAcronimo(@PathVariable("busca") String busca){
+        return institutoRepository.findByNomeIgnoreCaseContainingOrAcronimoIgnoreCaseContaining(busca, busca);
+    }
 }
---
+++ resumo_cientifico/src/main/java/ds1/grupo1/resumo_cientifico/controller/request/InstitutoRs.java
@@ -0,0 +1,29 @@
+package ds1.grupo1.resumo_cientifico.controller.request;
+
+public class InstitutoRs {
+    private Long id;
+    private String nome;
+    private String acronimo;
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAcronimo() {
+        return acronimo;
+    }
+    public void setAcronimo(String acronimo) {
+        this.acronimo = acronimo;
+    }
+
+    
+}
