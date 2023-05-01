d053dc219611bbcc2736396e6b8ed4146ba58f3f
Yago Frossard <yagofcc@hotmail.com>
2023-05-01 16:44:37 -0300

    Criadas queries para pesquisas

--- resumo_cientifico/src/main/java/ds1/grupo1/resumo_cientifico/controller/PesquisaController.java
+++ resumo_cientifico/src/main/java/ds1/grupo1/resumo_cientifico/controller/PesquisaController.java
@@ -11,11 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ds1.grupo1.resumo_cientifico.controller.request.PesquisaRs;
 import ds1.grupo1.resumo_cientifico.model.Pesquisa;
 import ds1.grupo1.resumo_cientifico.model.Pesquisador;
+import ds1.grupo1.resumo_cientifico.repository.InstitutoRepository;
 import ds1.grupo1.resumo_cientifico.repository.PesquisaRepository;
 import ds1.grupo1.resumo_cientifico.repository.PesquisadorRepository;
 
@@ -24,10 +26,12 @@ import ds1.grupo1.resumo_cientifico.repository.PesquisadorRepository;
 public class PesquisaController {
     private final PesquisadorRepository pesquisadorRepository;
     private final PesquisaRepository pesquisaRepository;
+    private final InstitutoRepository institutoRepository;
 
-    public PesquisaController(PesquisadorRepository pesquisadorRepository, PesquisaRepository pesquisaRepository) {
+    public PesquisaController(PesquisadorRepository pesquisadorRepository, PesquisaRepository pesquisaRepository, InstitutoRepository institutoRepository) {
         this.pesquisadorRepository = pesquisadorRepository;
         this.pesquisaRepository = pesquisaRepository;
+        this.institutoRepository = institutoRepository;
     }
 
     @GetMapping("/list")
@@ -54,8 +58,10 @@ public class PesquisaController {
         return irs;
     }
 
-    /*@GetMapping("/report/{anoInicio}/{anoFim}")
-    public List<PesquisaRs> getPesquisaByAno(@PathVariable("anoInicio") Integer ano1, @PathVariable("anoFim") Integer ano2) {
+    @GetMapping("/report/{anoInicio}/{anoFim}")
+    @ResponseBody
+    public List<PesquisaRs> getPesquisaByAnoBetween(@PathVariable("anoInicio") Integer ano1,
+            @PathVariable("anoFim") Integer ano2) {
         List<Pesquisa> pesquisas = pesquisaRepository.FindByAnoBetween(ano1, ano2);
 
         List<PesquisaRs> irs = new ArrayList<PesquisaRs>();
@@ -67,21 +73,25 @@ public class PesquisaController {
             p.setPaginaFinal(pesquisa.getPaginaFinal());
             p.setPaginaInicial(pesquisa.getPaginaInicial());
             p.setPeriodico(pesquisa.getPeriodico());
-            p.setPesquisadores(pesquisa.getPesquisadores());
             p.setTipo(pesquisa.getTipo());
             p.setVolume(pesquisa.getVolume());
+            for (Pesquisador prs : pesquisa.getPesquisadores()) {
+                p.getPesquisadores().add(prs.getNome());
+            }
             irs.add(p);
         }
 
         return irs;
     }
-    */
 
-    /*@GetMapping("/report/{anoInicio}/{anoFim}/{instituto}")
-    public List<PesquisaRs> getPesquisaByAnoAndInstituto(@PathVariable("anoInicio") Integer ano1, @PathVariable("anoFim") Integer ano2,
+    @GetMapping("/report/{anoInicio}/{anoFim}/{instituto}")
+    @ResponseBody
+    public List<PesquisaRs> getPesquisaByAnoAndInstituto(
+            @PathVariable("anoInicio") Integer ano1,
+            @PathVariable("anoFim") Integer ano2,
             @PathVariable("instituto") String instituto) {
-        List<Pesquisa> pesquisas = pesquisaRepository
-                .FindByAnoBetweenAndFindByInstitutoIgnoreCaseContaining(ano1, ano2, instituto);
+        
+        List<Pesquisa> pesquisas = pesquisaRepository.FindByAnoBetween(ano1, ano2);
 
         List<PesquisaRs> irs = new ArrayList<PesquisaRs>();
         for (Pesquisa pesquisa : pesquisas) {
@@ -92,19 +102,55 @@ public class PesquisaController {
             p.setPaginaFinal(pesquisa.getPaginaFinal());
             p.setPaginaInicial(pesquisa.getPaginaInicial());
             p.setPeriodico(pesquisa.getPeriodico());
-            p.setPesquisadores(pesquisa.getPesquisadores());
             p.setTipo(pesquisa.getTipo());
             p.setVolume(pesquisa.getVolume());
+            for (Pesquisador prs : pesquisa.getPesquisadores()) {
+                if(prs.getInstituto().getNome().equalsIgnoreCase(instituto)){
+                    p.getPesquisadores().add(prs.getNome());
+                }
+            }
+            if(p.getPesquisadores().isEmpty()) continue;
             irs.add(p);
         }
 
         return irs;
     }
-    */
+
+    @GetMapping("/report/{anoInicio}/{anoFim}/researcher/{pesquisador}")
+    @ResponseBody
+    public List<PesquisaRs> getPesquisaByAnoAndPesquisador(
+            @PathVariable("anoInicio") Integer ano1,
+            @PathVariable("anoFim") Integer ano2,
+            @PathVariable("pesquisador") String pesquisador) {
+        
+        List<Pesquisa> pesquisas = pesquisaRepository.FindByAnoBetween(ano1, ano2);
+
+        List<PesquisaRs> irs = new ArrayList<PesquisaRs>();
+        for (Pesquisa pesquisa : pesquisas) {
+            PesquisaRs p = new PesquisaRs();
+            p.setAno(pesquisa.getAno());
+            p.setId(pesquisa.getId());
+            p.setNome(pesquisa.getNome());
+            p.setPaginaFinal(pesquisa.getPaginaFinal());
+            p.setPaginaInicial(pesquisa.getPaginaInicial());
+            p.setPeriodico(pesquisa.getPeriodico());
+            p.setTipo(pesquisa.getTipo());
+            p.setVolume(pesquisa.getVolume());
+            for (Pesquisador prs : pesquisa.getPesquisadores()) {
+                if(prs.getNome().equalsIgnoreCase(pesquisador)){
+                    p.getPesquisadores().add(prs.getNome());
+                }
+            }
+            if(p.getPesquisadores().isEmpty()) continue;
+            irs.add(p);
+        }
+
+        return irs;
+    }
 
     @PostMapping("/")
     public void gravar(@RequestBody ArrayList<PesquisaRs> pesquisaRss) {
-        for( PesquisaRs pesquisaRs : pesquisaRss){
+        for (PesquisaRs pesquisaRs : pesquisaRss) {
             Pesquisa pesquisa = new Pesquisa();
             pesquisa.setAno(pesquisaRs.getAno());
             pesquisa.setNome(pesquisaRs.getNome());
@@ -113,17 +159,16 @@ public class PesquisaController {
             pesquisa.setPeriodico(pesquisaRs.getPeriodico());
             pesquisa.setTipo(pesquisaRs.getTipo());
             pesquisa.setVolume(pesquisaRs.getVolume());
-    
+
             List<Pesquisador> pesquisadores = pesquisadorRepository.findAll();
-            Set <Pesquisador> hashp = new HashSet<>();
+            Set<Pesquisador> hashp = new HashSet<>();
             for (String s : pesquisaRs.getPesquisadores()) {
                 for (Pesquisador p : pesquisadores) {
-                    if (p.getIdentificador_lattes().equals(s)){
-                        if(pesquisa.getPesquisadores() == null){
+                    if (p.getIdentificador_lattes().equals(s)) {
+                        if (pesquisa.getPesquisadores() == null) {
                             hashp.add(p);
                             pesquisa.setPesquisadores(hashp);
-                        }
-                        else{
+                        } else {
                             pesquisa.getPesquisadores().add(p);
                         }
                     }
@@ -134,10 +179,10 @@ public class PesquisaController {
     }
 
     @DeleteMapping("/{id}")
-    public void deletePesquisa(@PathVariable("id") Long id) throws Exception{
+    public void deletePesquisa(@PathVariable("id") Long id) throws Exception {
         var p = pesquisaRepository.findById(id);
 
-        if(p.isPresent()){
+        if (p.isPresent()) {
             Pesquisa pesquisa = p.get();
             pesquisaRepository.delete(pesquisa);
         } else {
